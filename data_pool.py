b1059a39daf3693244001ba2ee578e018c1e3a92
Bob Fu <yushiangfu@gmail.com>
2016-06-27 21:43:47 +0800

    Implement "data pool" module

--- data_pool.py
+++ data_pool.py
@@ -10,16 +10,22 @@ from os import path
 from bs4 import BeautifulSoup
 import pandas
 import numpy
+from enum import Enum
+
+class Type(Enum):
+    basic = 0
+
+class Param():
+    period = ""
 
 
-def need_update_stock_table():
-    if not os.path.exists('data/stock-table.csv'):
-        return True
-    return False
+def get_data(data_type, stock_no, param):
+    if data_type == Type.basic:
+        return get_basic(stock_no, param)
 
 
-def update_stock_table():
-    filename = 'data/stock-table.csv'
+def download_stock_table():
+    filename = 'data/stock_list.csv'
     url = 'http://www.emega.com.tw/js/StockTable.htm'
     with request.urlopen(url) as response, \
             open(filename, 'w') as out_file:
@@ -46,10 +52,97 @@ def update_stock_table():
         writer.writerows(res)
 
 
-def fetch_stock_table():
-    if need_update_stock_table():
-        update_stock_table()
-    return pandas.read_csv('data/stock-table.csv', 
+def get_basic(stock_no, param):
+    """
+    get basic data within specified date from file
+    download from url first if necessary
+    """
+    stock_no = str(stock_no)
+    addition = 0
+
+
+    res = str_to_dates(param.period)
+    if res != [False]:
+        start_dt, end_dt = res
+    else:
+        return [False]
+    res = str_to_dates(param.period, ignore_day=True)
+    if res != [False]:
+        start_month_dt, end_month_dt = res
+    else:
+        return [False]
+
+    # be compatible between twse/otc
+    if stock_no == 'index':
+        skip_rows = 1
+        date_label = '日期'
+        price_label = '發行量加權股價指數'
+        skip_footer = 1
+        engine = 'python'
+    elif stock_list.loc[stock_no]['TWSE/OTC'] == '上櫃':
+        skip_rows = 4
+        date_label = '日 期'
+        price_label = '收盤'
+        skip_footer = 1
+        engine = 'python'
+    else:
+        skip_rows = 1
+        date_label = '日期'
+        price_label = '收盤價'
+        skip_footer = 0
+        engine = 'c'
+
+    dt_iter = end_month_dt
+    df_list = []
+    empty_files = 0
+    while dt_iter >= start_month_dt or addition > 0 or not df_list:
+        filename = 'data/' + stock_no + '/' + 'price-' + str(dt_iter.year) + \
+            '-' + ('%02d') % (dt_iter.month) + '.csv'
+        if need_update_price(filename):
+            update_price(stock_no, dt_iter.year, dt_iter.month, filename)
+        if os.stat(filename).st_size == 0: # empty file
+            print(filename, 'is empty')
+            empty_files += 1
+        else:
+            try:
+                df = pandas.read_csv(filename, skiprows=skip_rows, 
+                                     index_col=date_label, date_parser=mg_to_ad, 
+                                     na_values=['--'], thousands=',', 
+                                     skipfooter=skip_footer, engine=engine)
+                df = df[pandas.notnull(df[price_label])] # to skip data with '--'
+                if len(df) == 0: # file with header and column names
+                    print(filename, 'has just header and column names')
+                    empty_files += 1        
+                else:
+                    addition -= len(df[df.index < str(start_dt)])
+                    df_list.append(df)
+            except ValueError: # file with just header... no column names
+                # data/3141/price-2014-03.csv would be false alarm
+                # but I don't want to handle varied footers just for it
+                print(filename, 'has just header')
+                empty_files += 1        
+                pass
+
+        if empty_files >= 3: # no data for half of a season?
+            return [False]
+        else:
+            dt_iter -= relativedelta.relativedelta(months=1)
+
+    odf = df = pandas.concat(df_list[::-1]) # concat data of different months
+    if len(df[str(start_dt):str(end_dt)]) == 0:
+        addition += 1 # ensure at least one data between start_dt~end_dt
+    df = df[-addition:] # truncate early data
+    df = df[df.index <= str(end_dt)] # truncate later data
+
+    #return [True, df]
+    return df
+    
+
+def get_stock_list():
+    """ 'update' should leave to 'download' module """
+    if not os.path.exists('data/stock_list.csv'):
+        download_stock_table()
+    return pandas.read_csv('data/stock_list.csv', 
                                    names=['Number', 'Name', 'TWSE/OTC', 'Catagory'], 
                                    index_col='Number').sort_index() # sort() is deprecated
 
@@ -123,7 +216,7 @@ def update_price(stock, year, month, filename):
                '/ch/trading/exchange/FMTQIK/FMTQIK2.php' +
                '?STK_NO=&myear=%(year)d&mmon=%(month)02d&type=csv') % \
                {'year': year, 'month': month}
-    elif stock_table.loc[stock]['TWSE/OTC'] == '上櫃':
+    elif stock_list.loc[stock]['TWSE/OTC'] == '上櫃':
         url = ('http://www.tpex.org.tw' + 
                '/web/stock/aftertrading/daily_trading_info/st43_download.php' +
                '?l=zh-tw&d=%(year)d/%(month)02d&stkno=%(stock)s&s=0,asc,0') % \
@@ -146,97 +239,14 @@ def update_price(stock, year, month, filename):
             return False
     return True
 
-
 def mg_to_ad(date_info):
     """ mg: 民國 """
     date_info = date_info.split('/')
     date_info[0] = str(int(date_info[0]) + 1911)
     return parser.parse('/'.join(date_info))
 
-
-def fetch_price(stock, period, addition):
-    """
-    fetch price data within date from file
-    download from url first if necessary
-    """
-    res = str_to_dates(period)
-    if res != [False]:
-        start_dt, end_dt = res
-    else:
-        return [False]
-    res = str_to_dates(period, ignore_day=True)
-    if res != [False]:
-        start_month_dt, end_month_dt = res
-    else:
-        return [False]
-
-    # be compatible between twse/otc
-    if stock == 'index':
-        skip_rows = 1
-        date_label = '日期'
-        price_label = '發行量加權股價指數'
-        skip_footer = 1
-        engine = 'python'
-    elif stock_table.loc[stock]['TWSE/OTC'] == '上櫃':
-        skip_rows = 4
-        date_label = '日 期'
-        price_label = '收盤'
-        skip_footer = 1
-        engine = 'python'
-    else:
-        skip_rows = 1
-        date_label = '日期'
-        price_label = '收盤價'
-        skip_footer = 0
-        engine = 'c'
-
-    dt_iter = end_month_dt
-    df_list = []
-    empty_files = 0
-    while dt_iter >= start_month_dt or addition > 0 or not df_list:
-        filename = 'data/' + stock + '/' + 'price-' + str(dt_iter.year) + \
-            '-' + ('%02d') % (dt_iter.month) + '.csv'
-        if need_update_price(filename):
-            update_price(stock, dt_iter.year, dt_iter.month, filename)
-        if os.stat(filename).st_size == 0: # empty file
-            print(filename, 'is empty')
-            empty_files += 1
-        else:
-            try:
-                df = pandas.read_csv(filename, skiprows=skip_rows, 
-                                     index_col=date_label, date_parser=mg_to_ad, 
-                                     na_values=['--'], thousands=',', 
-                                     skipfooter=skip_footer, engine=engine)
-                df = df[pandas.notnull(df[price_label])] # to skip data with '--'
-                if len(df) == 0: # file with header and column names
-                    print(filename, 'has just header and column names')
-                    empty_files += 1        
-                else:
-                    addition -= len(df[df.index < str(start_dt)])
-                    df_list.append(df)
-            except ValueError: # file with just header... no column names
-                # data/3141/price-2014-03.csv would be false alarm
-                # but I don't want to handle varied footers just for it
-                print(filename, 'has just header')
-                empty_files += 1        
-                pass
-
-        if empty_files >= 3: # no data for half of a season?
-            return [False]
-        else:
-            dt_iter -= relativedelta.relativedelta(months=1)
-
-    odf = df = pandas.concat(df_list[::-1]) # concat data of different months
-    if len(df[str(start_dt):str(end_dt)]) == 0:
-        addition += 1 # ensure at least one data between start_dt~end_dt
-    df = df[-addition:] # truncate early data
-    df = df[df.index <= str(end_dt)] # truncate later data
-    return [True, df]
-
-
 def char_to_slash(date_info):
     return datetime.strptime(date_info, '%Y年%m月')
-    
 
 def need_update_revenue(filename):
     """
@@ -309,4 +319,7 @@ def fetch_revenue(stock, period, addition):
     return [True, df]
 
 
-stock_table = fetch_stock_table()
+if not os.path.exists('data/'):
+    os.mkdir('data/')
+stock_list = get_stock_list()
+
